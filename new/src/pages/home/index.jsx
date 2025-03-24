d11367f280e011f6a44611db29cab2b88a5a9333
Filipe062 <luisfhylipe@gmail.com>
2025-03-24 12:29:50 -0300

    12:29 24/3

--- new/src/pages/home/index.jsx
+++ new/src/pages/home/index.jsx
@@ -1,12 +1,38 @@
 import { Link } from "react-router-dom";
 
-import { Header } from "../../components/Header"; 
+import  bunnerImage from "../../assets/bunner.jpg";
 
+import { Header } from "../../components/Header";
 import { Button } from "../../components/Button"
+
+import {
+    Container,
+    TextContent,
+    Title,
+    TitleHighLigth
+} from "./styles";
 
 const Home = () => {
     return ( <>
         <Header />
+        <Container>
+            <div>
+               <Title>
+                    <TitleHighLigth>
+                      Implemente
+                      <br />
+                    </TitleHighLigth>
+                    o seu futuro começa aqui !
+               </Title>
+               <TextContent>
+               React é uma biblioteca JavaScript popular desenvolvida pelo Facebook para a construção de interfaces de usuário, principalmente em aplicações web. Ela permite criar componentes reutilizáveis que atualizam de forma eficiente a interface quando os dados mudam.
+               </TextContent>
+               <Button title="Começar agora" variant="secondary" onClick={() => null} />
+            </div> 
+            <div>
+                <img src={bunnerImage} alt="imagem principal" />
+            </div>
+        </Container>
         </>)
 }
 
--- new/src/pages/login/index.jsx
+++ new/src/pages/login/index.jsx
@@ -1,11 +1,39 @@
-import { Link } from "react-router-dom";
+import bunnerImage from "../../assets/bunner.jpg"; // Correção da importação (sem o comentário)
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button"
+
+import { 
+  Container, 
+  TextContent, 
+  Title, 
+  TitleHighLigth 
+} from "./styles";
 
 const Login = () => {
-    return ( <>
-        <h1>Login</h1>
-        <Link to="/">Voltar para home</Link>
-        </>)
+  return (
+    <>
+      <Header />
+      <Container>
+        <div>
+          <Title>
+            <TitleHighLigth>
+              Implemente
+              <br />
+            </TitleHighLigth>
+            o seu futuro começa aqui!
+          </Title>
+          <TextContent>
+            React é uma biblioteca JavaScript popular desenvolvida pelo Facebook para a construção de interfaces de usuário, principalmente em aplicações web. Ela permite criar componentes reutilizáveis que atualizam de forma eficiente a interface quando os dados mudam.
+          </TextContent>
+          <Button title="Começar agora" variant="secondary" onClick={() => null} />
+        </div> 
+        <div>
+          <Input placeholder="email" />
+        </div>
+      </Container>
+    </>
+  );
 }
 
-
-export { Login }
+export { Login };
